d79f906707e5b95747edc30a52c3493b10eddddc
da99 <i-hate-spam-1234567@mailinator.com>
2017-04-10 23:31:49 -0500

    Dev. checkpoint.

--- src/galactic_trash.cr
+++ src/galactic_trash.cr
@@ -1,5 +1,20 @@
-require "./galactic_trash/*"
+# require "./galactic_trash/*"
+require "kemal"
+require "json"
 
-module GalacticTrash
-  # TODO Put your code here
+serve_static false
+
+get "/" do
+  "The home page."
 end
+
+get "/json" do | env |
+  env.response.content_type = "application/json"
+  {name: "World", type: "Hello"}.to_json
+end
+
+get "/hello" do
+  "Hello, World!"
+end
+
+Kemal.run
